734ee28df977b176bdac52513ca7941db3a195ce
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2025-06-25 12:24:11 -0300

    Create 23questão.c

---
+++ 23questão.c
@@ -0,0 +1,13 @@
+int main() {
+    float metros;
+
+    printf("Informe um valor em metros: ");
+    scanf("%f", &metros);
+
+    printf("\nCONVERSÂO:\n");
+    printf("Decímetros: %.2f dm\n", metros * 10);
+    printf("Centímetros: %.2f cm\n", metros * 100);
+    printf("Milímetros: %.2f mm\n", metros * 1000);
+
+    return 0;
+}
