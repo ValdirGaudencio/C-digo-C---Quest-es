73018463a69dc61b39a958693d9df056c5226306
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2025-08-11 12:09:04 -0300

    Create questao59.c

---
+++ questao59.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   
+    int num[5], i;
+    
+    num[0] = 10;
+    num[1] = 20;
+    num[2] = 30;
+    num[3] = 40;
+    num[4] = 50;
+    
+    for( i = 0; i < 5; i++){
+        printf("Valor do vetor na posição %d: %d\n", i, num[i]);
+    }
+
+    return 0;
+}
