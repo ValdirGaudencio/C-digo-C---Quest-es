60f5b2308ecd761897ac3d2983ce732c057f5a96
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2025-08-26 12:59:11 -0300

    Update 64questao.c

--- 64questao.c
+++ 64questao.c
@@ -5,7 +5,7 @@ int main() {
     int i, j;
 
    
-    printf("Digite os elementos da matriz 3x3:\n");
+    printf("Escreva os elementos da matriz 3x3:\n");
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
             printf("Elemento [%d][%d]: ", i, j);
