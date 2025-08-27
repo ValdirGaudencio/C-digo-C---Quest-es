c1aa33496c274249c50ca3a33bd94e3f17a9a5c7
Gaudencio <164903641+ValdirGaudencio@users.noreply.github.com>
2025-08-27 12:47:31 -0300

    Create 69questao.c

---
+++ 69questao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+int matriz[2][3] = {
+    {3, 2, 1},
+    {1, 2, 3}
+};
+
+for(int i = 0; i < 2; i++){
+    for(int j = 0; j < 3; j++){
+        printf("%d ", matriz[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
